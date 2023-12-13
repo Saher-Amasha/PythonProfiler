0cb07ea994abb375e3cb68123fbbb93671f7ee4f
Saher-Amasha <saheramasha0@gmail.com>
2023-12-13 23:20:09 +0200

    init main program

---
+++ main.py
@@ -0,0 +1,14 @@
+import tkinter
+
+def main():
+    root = tkinter.Tk()
+    myLabel = tkinter.Label(root,text="python Profiler")
+    myLabel.pack()
+    # Event loop
+    
+    root.mainloop()
+    print("Hello World!")
+
+
+if __name__ == "__main__":
+    main()
